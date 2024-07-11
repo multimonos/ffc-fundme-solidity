d0d3555c21fdd75d249775452cdf996b39ccefbb
Craig Hopgood <searaig@gmail.com>
2024-07-11 17:09:20 -0400

    replace hoax() with vm.deal()

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {ConfigManager} from "../script/ConfigManager.s.sol";
 
 contract FundMeTest is Test {
 
     FundMe public ins;
-    DeployFundMe deployer;
+    DeployFundMe public deployer;
 
     address public owner = makeAddr("owner");
     address public funder = makeAddr("funder");
@@ -20,6 +21,14 @@ contract FundMeTest is Test {
         ins = deployer.run();
     }
 
+    function test_addresses() public view {
+        console.log("deployer: %s", address(deployer));
+        console.log("msg.sender: %s", address(msg.sender));
+        console.log("this: %s", address(this));
+        console.log("ins.owner: %s", address(ins.owner()));
+        assertTrue(true);
+    }
+
     function test_ini_balance_zero() public view {
         assertEq(address(ins).balance, 0);
     }
@@ -29,17 +38,18 @@ contract FundMeTest is Test {
     }
 
     function test_owner_is_sender() public view {
-        // FundMeTest is owner.
-        assertEq(address(deployer), ins.owner());
+        // there is a default msg.sender controlled by foundry
+        assertEq(address(msg.sender), ins.owner());
     }
 
-    function test_price_feed_version() public view {
-        assertEq(ins.getFeedVersion(), 4);
-    }
+//    function test_price_feed_version() public view {
+//        assertEq(ins.getFeedVersion(), 4);
+//    }
 
     function test_fund_requires_min_eth() public {
-        hoax(funder, 10 ether);
         vm.expectRevert();
+        vm.deal(funder, 10 ether);
+        vm.prank(funder);
         ins.fund{value: 1 wei}();
     }
 
@@ -48,52 +58,64 @@ contract FundMeTest is Test {
         assertEq(address(ins).balance, 2e15 wei);
     }
 
-//    function test_funder_attribution() public {
-//        startHoax(funder, 10 ether);
-//        ins.fund{value: 2 ether}();
-//        ins.fund{value: 2 ether}();
-//        vm.stopPrank();
-//
-//        assertEq(funder.balance, 6 ether);
-//        assertEq(ins.funderAmount(funder), 4 ether);
-//    }
-//
-//    function test_withdraw() public {
-//
-//        startHoax(funder, 10 ether);
-//        ins.fund{value: 2 ether}();
-//        ins.fund{value: 3 ether}();
-//        ins.fund{value: 1 ether}();
-//        vm.stopPrank();
-//
-//        assertEq(address(ins).balance, 6 ether);
-//        assertEq(address(funder).balance, 4 ether);
-//
-////        vm.prank(address(this));
-//        ins.withdraw();
-//
-//        assertEq(address(ins).balance, 0, "contract balance is zero");
-//        assertEq(address(this).balance, 6 ether, "owner receives entire balance");
-//        assertEq(ins.funderAmount(funder), 0, "funder balance reset");
-//    }
-//
-//    function test_withdraw_only_owner() public {
-//        vm.prank(address(7));
-//        vm.expectRevert();
-//        ins.withdraw();
-//    }
-//
-//    function test_payable_fallback_exists() public {
-//        address foobar = makeAddr("foobar");
-//        hoax(foobar, 10 ether);
-//        address(ins).call{value: 5 ether}("");
-//        assertEq(address(ins).balance, 5 ether);
-//    }
-//
-//    function test_fallback_funder_attribution() public {
-//        address foobar = makeAddr("foobar");
-//        hoax(foobar, 10 ether);
-//        address(ins).call{value: 5 ether}("");
-//        assertEq(ins.funderAmount(foobar), 5 ether);
+    function test_funder_attribution() public {
+        vm.deal(funder, 10 ether);
+        vm.startPrank(funder);
+        ins.fund{value: 2 ether}();
+        ins.fund{value: 2 ether}();
+        vm.stopPrank();
+
+        assertEq(funder.balance, 6 ether);
+        assertEq(ins.funderAmount(funder), 4 ether);
+    }
+
+    function test_only_owner_can_withdraw() public {
+        vm.prank(address(7));
+        vm.expectRevert();
+        ins.withdraw();
+    }
+
+    function test_withdraw() public {
+        vm.deal(funder, 10 ether);
+        vm.startPrank(funder);
+        ins.fund{value: 2 ether}();
+        ins.fund{value: 3 ether}();
+        ins.fund{value: 1 ether}();
+        vm.stopPrank();
+
+        assertEq(address(ins).balance, 6 ether);
+        assertEq(address(funder).balance, 4 ether);
+
+        uint256 ogbalance = msg.sender.balance;
+        vm.prank(msg.sender);
+        ins.withdraw();
+        assertEq(address(msg.sender), address(ins.owner()));
+
+        assertEq(address(ins).balance, 0, "contract balance is zero");
+        assertEq(ins.funderAmount(funder), 0, "funder balance reset");
+        assertEq(address(msg.sender).balance, ogbalance + 6 ether, "owner receives entire balance");
+    }
+
+
+    function test_payable_fallback_exists() public {
+        address foobar = makeAddr("foobar");
+        vm.deal(foobar, 10 ether);
+        vm.prank(foobar);
+        address(ins).call{value: 5 ether}("anything()");
+        assertEq(address(ins).balance, 5 ether);
+    }
+
+    function test_fallback_funder_attribution() public {
+        address foobar = makeAddr("foobar");
+        vm.deal(foobar, 10 ether);
+        vm.prank(foobar);
+        address(ins).call{value: 5 ether}("");
+        assertEq(ins.funderAmount(foobar), 5 ether);
+    }
+
+//    function test_get_price() public {
+//        ConfigManager mgr = new ConfigManager();
+//        console.log("price: %s", mgr.DEFAULT_PRICE());
+//        assertTrue(true);
 //    }
 }
